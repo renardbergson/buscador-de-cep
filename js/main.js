9919bab843d691cd2fe8b49f6e2305aa663febb1
Renard Bergson <renardrock@gmail.com>
2022-11-10 21:26:26 -0300

    feat: :sparkles: introduzindo verificação de cep inválido

--- js/main.js
+++ js/main.js
@@ -3,9 +3,20 @@ const $searchingScreen = document.querySelector('.searchingScreen')
 
 const $cepInput = document.querySelector('#cepInput')
 const $searchBtn = document.querySelector('#searchBtn')
+const $toast = document.querySelector('.toast')
 
 $searchBtn.onclick = e => {
     e.preventDefault()
+
+    if ($cepInput.length !== 8) {
+        $toast.classList.add('visible')
+
+        setTimeout(() => {
+            $toast.classList.remove('visible')
+        }, 2000)
+        
+        return
+    }
 
     $initialScreen.style.display = 'none'
     $searchingScreen.style.display = 'block'
